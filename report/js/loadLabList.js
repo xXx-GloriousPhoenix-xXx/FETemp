48cd3bfcdf99e76da498ec49ac54d091d5a40653
RomanPR <prikhodjko.roman@gmail.com>
2025-11-03 14:04:15 +0200

    initial mobile

--- report/js/loadLabList.js
+++ report/js/loadLabList.js
@@ -1,16 +1,22 @@
-const labCount = 9;
+const loadLabList = (currentLab) => {
+    const labCount = 9;
 
-const labList = document.getElementById("lablist");
+    const labList = document.getElementById("lablist");
 
-for (let labNumber = 1; labNumber <= labCount; labNumber++) {
-    const labLink = document.createElement('a');
-    labLink.href = `./lab${labNumber}.html`;
-    labLink.textContent = `Практична №${labNumber}`;
-    labLink.classList.add('link');
+    for (let labNumber = 1; labNumber <= labCount; labNumber++) {
+        const labLink = document.createElement('a');
+        labLink.href = `./lab${labNumber}.html`;
+        labLink.textContent = `Практична №${labNumber}`;
+        labLink.classList.add('link');
 
-    const labLinkWrapper = document.createElement('div');
-    labLinkWrapper.classList.add(['wrapper', 'link']);
+        const labLinkWrapper = document.createElement('div');
+        labLinkWrapper.classList.add('wrapper', 'link');
 
-    labLinkWrapper.appendChild(labLink);
-    labList.appendChild(labLinkWrapper);
+        if (labNumber === currentLab) {
+            labLinkWrapper.classList.add('active');
+        }
+
+        labLinkWrapper.appendChild(labLink);
+        labList.appendChild(labLinkWrapper);
+    }
 }
